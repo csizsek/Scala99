156a07966739b37d1353c8be346915bd404c81a7
Peter Csizsek <peter.csizsek@gmail.com>
2016-09-13 15:59:13 +0200

    emoved extra braces

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -3,22 +3,18 @@ import scala.annotation.tailrec
 object Lists {
 
   @tailrec
-  def last(l: List[Int]): Option[Int] = {
-    l match {
-      case Nil => None
-      case x :: Nil => Some(x)
-      case x :: xs => last(xs)
-    }
+  def last(l: List[Int]): Option[Int] = l match {
+    case Nil => None
+    case x :: Nil => Some(x)
+    case x :: xs => last(xs)
   }
 
   @tailrec
-  def penultimate(l: List[Int]): Option[Int] = {
-    l match {
-      case Nil => None
-      case _ :: Nil => None
-      case x :: _ :: Nil => Some(x)
-      case x :: xs => penultimate(xs)
-    }
+  def penultimate(l: List[Int]): Option[Int] = l match {
+    case Nil => None
+    case _ :: Nil => None
+    case x :: _ :: Nil => Some(x)
+    case x :: xs => penultimate(xs)
   }
 
 }
